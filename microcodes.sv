cd779a0171e31a13c3fe9cbbec583bd1690580c9
SidK123 <83468167+SidK123@users.noreply.github.com>
2024-04-17 22:31:45 -0400

    fixes microcodes to pass TT06 flow

--- microcodes.sv
+++ microcodes.sv
@@ -8,7 +8,7 @@ module microcodes(
   output logic        alu_src1_load_en, alu_src2_load_en, sel_field_load_en
 );
 
-  enum logic [7:0] {IDLE, INSTR_FETCH, ARITH_OP_1, ARITH_OP_2, ARITH_OP_3, ARITH_OP_3A, ARITH_OP_4, ARITH_OP_5, ARITH_OP_6, ARITH_OP_7, ARITH_OP_8} cState, nState;
+  enum logic [7:0] {IDLE, ARITH_OP_1, ARITH_OP_2, ARITH_OP_3, ARITH_OP_3A, ARITH_OP_4, ARITH_OP_5, ARITH_OP_6, ARITH_OP_7} cState, nState;
 
   always_ff @(posedge clock, negedge reset_n) begin
     if(~reset_n) begin
@@ -42,7 +42,12 @@ module microcodes(
         sel_field_load_en = 1'b1;
       end
       ARITH_OP_2: begin
-        nState            = imm_instruction ? ARITH_OP_3A : ARITH_OP_3;
+        if(imm_instruction) begin
+          nState = ARITH_OP_3A;
+        end
+        else begin
+          nState = ARITH_OP_3;
+        end
         data_bus_sel      = RF;
         pc_load_en        = 1'b0;
         ir_load_en        = 1'b0;
@@ -93,7 +98,7 @@ module microcodes(
       end
      ARITH_OP_6: begin
         nState            = ARITH_OP_7;
-        data_bus_sel      = ALU;
+        data_bus_sel      = ALU_BUS;
         pc_load_en        = 1'b0;
         ir_load_en        = 1'b0;
         rf_write_read     = 1'b1;
@@ -105,6 +110,16 @@ module microcodes(
         nState            = IDLE;
         data_bus_sel      = PC_PLUS_4;
         pc_load_en        = 1'b1;
+        ir_load_en        = 1'b0;
+        rf_write_read     = 1'b0;
+        alu_src1_load_en  = 1'b0;
+        alu_src2_load_en  = 1'b0;
+        sel_field_load_en = 1'b0;
+     end
+     default: begin
+        nState            = IDLE;
+        data_bus_sel      = ZERO;
+        pc_load_en        = 1'b0;
         ir_load_en        = 1'b0;
         rf_write_read     = 1'b0;
         alu_src1_load_en  = 1'b0;
